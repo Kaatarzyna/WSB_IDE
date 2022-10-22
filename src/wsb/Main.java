c096ff858ec3303dd74e9991be8e09ea5aa98530
Katarzyna Duchowska <k.duchowska@o2.pl>
2022-10-22 09:08:24 +0200

    zmiany zajęcia 22.10.2022

--- src/wsb/Main.java
+++ src/wsb/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("!!!");
         System.out.println("Hej! Podaj tryb liczenia (r|p) i indeks liczby w ciągu Fibonacciego, np. r7. Wyjście - q");
 
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +18,7 @@ public class Main {
             FibonacciFactory factory = new FibonacciFactory(line);
 
             if (factory.mode == null) {
-                System.out.println("Niepoprawne wejście! Spróbuj ponownie");
+                System.out.println("Niepopradddwne wejście! Spróbuj ponownie");
                 continue;
             }
 
